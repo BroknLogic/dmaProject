fb84c87f5c9b41bc694f8d1dcac1c2462e57c07c
Crashprn <thewall907@gmail.com>
2023-04-05 14:39:53 -0700

    Actually uploading it this time

--- basic_travese.py
+++ basic_travese.py
@@ -1,113 +1,5 @@
-# imports
-import dash
-import visdcc
-import pandas as pd
-import numpy as np
-from dash import dcc
-from dash import html
-from dash.dependencies import Input, Output, State
-import random
 from Node import Node
-from collections import deque
-
-
-def realPath(source, target, q):
-    # Make a real path to from source to target
-    # poll Q values and update q graph
-    # return real time
-    pass
-
-
-
-def getNodes(nodeCount: int):
-    nodes = [Node(str(i)) for i in range(nodeCount)]
-    dashNodes = [{'id': str(i), 'label': str(i), 'shape': 'dot', 'size': 7} for i in range(nodeCount)]
-    return nodes, dashNodes
-
-def getEdges(nodes: list[Node], extraEdges: int):
-    nodesAdded = []
-    edges = []
-    for node in nodes:
-        if len(nodesAdded) == 0:
-            nodesAdded.append(node.id)
-        else:
-            source, target = random.choice(nodesAdded), node.id
-            mean = random.randrange(1, 7)
-            stdDev = random.randrange(1, 7)
-            edge = {
-                'id': source + "__" + target,
-                'from': source,
-                'to': target,
-                'width': mean, 
-                'mean': mean,
-                'stdDev': stdDev
-            }
-            edge2 = {
-                'id': target + "__" + source,
-                'from': target,
-                'to': source,
-                'width': mean, 
-                'mean': mean,
-                'stdDev': stdDev
-            }
-            nodes[int(source)].addEdge(edge)
-            nodes[int(target)].addEdge(edge2)
-            edges.append(edge)
-            nodesAdded.append(node.getId())
-    edgeNum = len(edges)
-    while len(edges) < edgeNum + extraEdges:
-        source, target = random.choice(nodesAdded), random.choice(nodesAdded)
-        mean = random.randrange(1, 7)
-        edge = {
-            'id': source + "__" + target,
-            'from': source,
-            'to': target,
-            'width': mean, 
-            'mean': mean,
-            'stdDev': random.randrange(1, 7)
-        }
-        edge2 = {
-            'id': target + "__" + source,
-            'from': target,
-            'to': source,
-            'width': mean, 
-            'mean': mean,
-            'stdDev': stdDev
-        }
-        contains = False
-        for testEdge in edges:
-            if testEdge['id'] == edge['id'] or testEdge['to'] + '__' + testEdge['from'] == edge['id']:
-                contains = True
-        if not contains and source != target:
-            edges.append(edge)
-            nodes[int(source)].addEdge(edge)
-            nodes[int(target)].addEdge(edge2)
-    return edges
-
-
-def visualizeNetwork(nodes, edges):
-    app = dash.Dash()
-
-    # define layout
-    app.layout = html.Div([
-        visdcc.Network(id = 'net', 
-                        data = {'nodes': nodes, 'edges': edges},
-                        options = dict(height= '600px', width= '100%')),
-        dcc.RadioItems(id = 'color',
-                        options=[{'label': 'Red'  , 'value': '#ff0000'},
-                                {'label': 'Green', 'value': '#00ff00'},
-                                {'label': 'Blue' , 'value': '#0000ff'} ],
-                        value='Red'  )             
-                        ])
-
-    @app.callback(
-    Output('net', 'options'),
-    [Input('color', 'value')])
-    def myfun(x):
-        return {'nodes':{'color': x}}
-    
-    app.run_server(debug=True)
-
+from Graph import Graph
 
 
 def breadth_first(Nodes: list[Node]):
@@ -138,19 +30,14 @@ def breadth_first(Nodes: list[Node]):
 
 def main():
     nodeCount = 20
-    nodes, dashNodes = getNodes(nodeCount)
-
     extraEdges = 20
-    edges = getEdges(nodes, extraEdges)
-    for node in nodes:
-        for edge in node.getEdges():
-            print(edge)
-    try:
-        print(breadth_first(nodes))
-    except:
-        pass
+    graph = Graph(nodeCount, extraEdges)
+    nodes = graph.getNodes(nodeCount)
+    edges = graph.getEdges(nodes, extraEdges)
     
-    visualizeNetwork(dashNodes, edges)
+    print(breadth_first(nodes))
+    
+    graph.visualizeNetwork(graph.getDashNodes(), edges)
 
     
 # define main calling
