89665cc9d7d463ad6dabfce01894ebce65741c49
BroknLogic <jaromburr@hotmail.com>
2023-04-12 13:43:16 -0600

    delivery list, new graphing system

--- Graph.py
+++ Graph.py
@@ -1,5 +1,6 @@
 import dash
 import visdcc
+import dash_cytoscape as cyto
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output, State
@@ -16,6 +17,7 @@ class Graph:
         self.nodes, self.dashNodes = self.makeNodes(nodeCount)
         self.edges: list[Edge] = self.makeEdges(self.nodes, extraEdges)
         self.random = np.random.normal
+        self.location = 0
     
     
     def samplePath(self, path: list[str]) -> list[float]:
@@ -39,7 +41,7 @@ class Graph:
     def getNodes(self) -> list[Node]:
         return self.nodes
     
-    def getDashNodes(self) -> dict[str,str]:
+    def getDashNodes(self, location) -> dict[str,str]:
         return self.dashNodes
     
     def getEdges(self) -> dict[str, str]:
@@ -47,7 +49,7 @@ class Graph:
     
     def makeNodes(self, nodeCount: int) -> tuple[list[Node], dict[str,str]]:
         nodes = [Node(str(i)) for i in range(nodeCount)]
-        dashNodes = [{'id': str(i), 'label': str(i), 'shape': 'dot', 'size': 7} for i in range(nodeCount)]
+        dashNodes = [{'data': {'id': str(i), 'label': str(i)}, 'classes':'location' if i == 0 else ''} for i in range(nodeCount)]
         return nodes, dashNodes
 
     def makeEdges(self, nodes: list[Node], extraEdges: int, len_range: tuple[int, int] = (5, 20)) -> dict[str, str]:
@@ -82,7 +84,7 @@ class Graph:
                 nodes[int(source)].addEdge(edge)
                 nodes[int(target)].addEdge(edge2)
         return edges
-    
+
 
     def getDeliveries(self, numDelivieries: int) -> dict[str,int]:
         deliveries = {}
@@ -95,22 +97,55 @@ class Graph:
     def visualizeNetwork(self) -> None:
         app = dash.Dash()
 
+        default_stylesheet = [
+    {
+        'selector': 'node',
+        'style': {
+            'background-color': '#BFD7B5',
+            'label': 'data(label)'
+        }
+    },
+    {
+        'selector': 'edge',
+        'style': {
+            'line-color': '#A3C4BC'
+        }
+    },
+    {
+        'selector': '.location',
+        'style': {
+            'background-color': 'red',
+            'line-color': 'red'
+        }
+    }
+]
+
         # define layout
         app.layout = html.Div([
-            visdcc.Network(id = 'net', 
-                            data = {'nodes': self.dashNodes, 'edges': [edge.toDict() for edge in self.edges]},
-                            options = dict(height= '600px', width= '100%')),
-            dcc.RadioItems(id = 'color',
-                            options=[{'label': 'Red'  , 'value': '#ff0000'},
-                                    {'label': 'Green', 'value': '#00ff00'},
-                                    {'label': 'Blue' , 'value': '#0000ff'} ],
-                            value='Red'  )             
-                            ])
+            cyto.Cytoscape(
+                id='net',
+                layout={'name': 'breadthfirst'},
+                stylesheet=default_stylesheet,
+                style={'width': '100%', 'height': '400px'},
+                elements= self.dashNodes + [edge.toDict() for edge in self.edges]
+            ),
+            html.Button('Submit', id='submit-val', n_clicks=0),
+        ])
 
-        @app.callback(
-        Output('net', 'options'),
-        [Input('color', 'value')])
-        def myfun(x):
-            return {'nodes':{'color': x}}
+        @app.callback(Output('net', 'elements'),
+              Input('submit-val', 'n_clicks'))
+        def update_elements(button):
+            dashNodes = self.dashNodes
+            self.location += 1
+            if self.location > len(dashNodes) - 1:
+                self.location = 0
+            for i in range(len(dashNodes)):
+                if i == self.location:
+                    dashNodes[i]['classes'] += 'location'
+                else:
+                    dashNodes[i]['classes'] = ''
+            elements= self.dashNodes + [edge.toDict() for edge in self.edges]
+            return elements
+
         
         app.run_server(debug=True)
