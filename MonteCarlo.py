ce27f16ffbe623cf7db630b6df680334c03cd11f
Crashprn <thewall907@gmail.com>
2023-04-26 09:07:08 -0700

    Monte Carlo added

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -1,62 +1,67 @@
 import numpy as np
 from Graph import Graph
+from Node import Node
 
 class MonteCarlo:
 
-    def __init__(self, graph: Graph, qMatrix: np.ndarray[np.ndarray[float]], gamma: float = 0.01):
-        self.graph = graph
-        self.qMatrix = qMatrix
-        self.useMatrix = np.zeros_like(qMatrix)
-        self.gamma = gamma
-        self.visitedPaths = []
+    def __init__(self, graph: Graph, numberOfParticles: int, depotNode: str = '0'):
+        self.graph: Graph = graph
+        self.numberOfParticles = numberOfParticles
+        self.depot = depotNode
     
-    def getRealPath(self, target: str, source: str, path_dict: dict[str, list[str]]) -> list[str]:
+    def breadth_first(self, source, target, Nodes: list[Node]):
+        queue = []
+        visited = {}
+        visited[source.getId()] =  np.inf
+        queue.append(source)
+    
+        while (len(queue)> 0):
+            current = queue.pop(0) 
+            for edge in current.getEdges():
+                to_node = edge.getTarget()
+                if to_node not in visited:
+                    queue.append(Nodes[int(to_node)])
+                    visited[to_node] = current.getId()
+    
         path = [target]
-        used_edges = []
-        while path[-1] != source:
-            # get all of the edges for the current node
-            node_edges = [edge for edge in self.graph.getNodes()[int(path[-1])].getEdges()]
-            # remove an edge if it was already traversed
-            for edge in node_edges:
-                if edge.getId() in used_edges:
-                    node_edges.remove(edge)
-            # pick edge and add target to path
-            edge = np.random.choice(node_edges)
-            used_edges.append(edge.getId())
-            path.append(edge.getTarget())
+    
+        while path[-1] != source.getId():
+            path.append(visited[path[-1]])
+    
         path.reverse()
-        return path
+        return path 
 
-    def updateQMatrix(self) -> None:
-        for path in self.visitedPaths:
-            path_sample = self.graph.samplePath(path)
-            for i in range(len(path)-2):
-                # increment the use matrix
-                self.useMatrix[int(path[i])][int(path[i+1])] += 1
-                # update the q matrix
-                n = self.useMatrix[int(path[i])][int(path[i+1])]
-                old_val = self.qMatrix[int(path[i])][int(path[i+1])]
-                self.qMatrix[int(path[i])][int(path[i+1])] = (old_val * (n - 1) + path_sample[i]) / n
-        self.visitedPaths = []
-
-    def findOptimalPath(self, source: str, target: str, num_episodes: int) -> list[str]:
-        for i in range(num_episodes):
-            path = self.getRealPath(target, source, {})
-            self.visitedPaths.append(path)
-        self.updateQMatrix()
-        return self.getOptimalPath(source, target)
-
-    def getOptimalPath(self, source: str, target: str) -> list[str]:
-        path = [source]
-        while path[-1] != target:
-            node = int(path[-1])
-            edges = self.graph.getNodes()[node].getEdges()
-            max_val = float('-inf')
-            max_edge = None
-            for edge in edges:
-                val = self.qMatrix[node][int(edge.getTarget())]
-                if val > max_val:
-                    max_val = val
-                    max_edge = edge
-            path.append(max_edge.getTarget())
-        return path
+    def particleDay(self, packages, timeLimit):
+        totalProfit = 0
+        totalTime = 0
+        
+        while totalTime < timeLimit:
+            # Get a random package
+            package = np.random.choice(packages)
+            # Get the path from the package
+            path = self.breadth_first(self.depot, package[0], self.graph.getNodes())
+            # Get the time it takes to travel the path
+            time = 2 * sum(self.graph.samplePath(path))
+            # Get the profit of the package
+            
+            if totalTime + time > timeLimit:
+                break
+            
+            
+            # Add the time and profit to the total
+            totalTime += time
+            totalProfit += package[1]
+        
+        return totalProfit
+        
+        
+        
+    
+    def simulateDay(self, packages, timeLimit):
+        profit = []
+        
+        for _ in range(self.numberOfParticles):
+            particleProfit = self.particleDay(packages, timeLimit)
+            profit.append(particleProfit)
+        
+        return profit[np.argmax(profit)]
